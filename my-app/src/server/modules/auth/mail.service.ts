69bea2e1dc0adb5d57784d19b5dca235a2dc2d1e
Georgii <grevermain@gmail.com>
2026-05-12 00:20:20 +0300

    disable smtp send

--- my-app/src/server/modules/auth/mail.service.ts
+++ my-app/src/server/modules/auth/mail.service.ts
@@ -3,28 +3,28 @@ import nodemailer from 'nodemailer'
 export const sendEmail = async (to: string, code: string) => {
     console.log('EMAIL:', process.env.EMAIL_USER)
 console.log('PASS:', process.env.EMAIL_PASS)
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
 
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
-  }
-})
-
-  await transporter.sendMail({
-    from: `"MyShop" <${process.env.EMAIL_USER}>`,
-    to,
-    subject: 'Код подтверждения',
-    html: `
-      <div style="font-family:sans-serif">
-        <h2>Подтверждение регистрации</h2>
-        <p>Ваш код:</p>
-        <h1>${code}</h1>
-      </div>
-    `
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
   })
+
+  // await transporter.sendMail({
+  //   from: `"MyShop" <${process.env.EMAIL_USER}>`,
+  //   to,
+  //   subject: 'Код подтверждения',
+  //   html: `
+  //     <div style="font-family:sans-serif">
+  //       <h2>Подтверждение регистрации</h2>
+  //       <p>Ваш код:</p>
+  //       <h1>${code}</h1>
+  //     </div>
+  //   `
+  // })
 
 }
