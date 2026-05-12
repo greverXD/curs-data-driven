5806e3e037ff9bc0575abd2bc3a625f40e8e5610
Georgii <grevermain@gmail.com>
2026-05-12 11:35:08 +0300

    Front header

--- my-app/src/widgets/HeroBanner.vue
+++ my-app/src/widgets/HeroBanner.vue
@@ -1,24 +1,25 @@
 <template>
   <section
-    class="relative h-[500px] flex items-center justify-center text-white"
+    class="relative h-[350px] md:h-[500px] flex items-center justify-center text-white overflow-hidden"
   >
-
     <img
       src="https://cdn.ranvik.ru/ranvik/lenta/content/39/9a/399adfcb0970c870d27173cd4040afb3_preview.webp"
-      class="absolute w-full h-full object-cover"
+      class="absolute inset-0 w-full h-full object-cover"
     />
 
     <div class="absolute inset-0 bg-black/40"></div>
 
-    <div class="relative text-center">
-      <h2 class="text-4xl font-bold mb-4">Новая коллекция</h2>
+    <div class="relative text-center px-4">
+      <h2 class="text-3xl md:text-5xl font-bold mb-4">
+        Новая коллекция
+      </h2>
 
-      <button
-        class="px-6 py-3 bg-white text-black rounded transition 
-               hover:scale-110 hover:bg-gray-200"
+      <router-link
+        to="/catalog"
+        class="inline-block px-6 py-3 bg-white text-black rounded transition hover:scale-105 hover:bg-gray-200"
       >
-        <router-link to="/catalog">В каталог</router-link>
-      </button>
+        В каталог
+      </router-link>
     </div>
   </section>
 </template>
